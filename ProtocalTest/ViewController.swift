a7b12ceb6248db84272dbbdfebbef186d59d8457
Audreyw <q344wang@edu.uwaterloo.ca>
2018-09-21 17:53:39 -0400

    Initial Commit

---
+++ ProtocalTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProtocalTest
+//
+//  Created by Audrey Wang on 2018-09-21.
+//  Copyright © 2018 Audrey Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
